d7155def698cfc56b8e9870d113990d8837f77b4
AnuradhaDGunasinghe <anuradha@alsakbcomputer.com>
2023-03-06 12:53:54 +0400

    inventory task update

--- presentation/inventory/printing/pimage.php
+++ presentation/inventory/printing/pimage.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 require_once "phpqrcode/qrlib.php";
+$inventory_id=0;
+$machine_id=0;
+$keyboard_backlight=0;
 $brand = trim($_POST['brand']);
 $core=trim($_POST['core']);
 $generation=trim($_POST['gen']);
@@ -15,13 +18,13 @@ $speed=trim($_POST['speed']);
 $lcd_size=trim($_POST['lcd_size']);
 $resolution=trim($_POST['resolution']);
 $dvd=trim($_POST['dvd']);
-$keyboard_backlight=trim($_POST['keyboard_backlight']);
-$machine_id=trim($_POST['machine_id']);
+if(empty($_POST['keyboard_backlight'])){}else{$keyboard_backlight=trim($_POST['keyboard_backlight']);}
+if(empty($_POST['machine_id'])){}else{$machine_id=trim($_POST['machine_id']);}
+if(empty($_POST['inventory_id'])){}else{$inventory_id=trim($_POST['inventory_id']);}
 $user_id=$_SESSION['user_id'];
-
-$connection = mysqli_connect("localhost", "root", "", "wms");
-
-$query = "INSERT INTO `warehouse_information_sheet`(
+$connection = mysqli_connect("localhost", "root", "", "main_project");
+if($inventory_id==0){
+$query = "INSERT INTO `main_inventory_informations`(
             `device`,
             `processor`,
             `core`,
@@ -30,13 +33,13 @@ $query = "INSERT INTO `warehouse_information_sheet`(
             `brand`,
             `create_by_inventory_id`,
             `mfg`,
-            `machine_from_supplier_id`,
+            `machine_id`,
             `series`,
             `speed`,
             `battery`,
             `lcd_size`,
-            `touch_or_non_touch`,
-             `dvd`,
+            `touch_or_none_touch`,
+             `optical`,
              screen_resolution,
              location
         )
@@ -60,11 +63,13 @@ $query = "INSERT INTO `warehouse_information_sheet`(
             'WH2'
         )";
     $query1 = mysqli_query($connection, $query);
-    $sql="UPDATE machine_from_supplier SET add_to_wis='1' WHERE machine_id='$machine_id'";
+    echo  $query;
+    $sql="UPDATE machine_from_suppliers SET add_to_inventory='1' WHERE machine_id='$machine_id'";
     $sql_run=mysqli_query($connection,$sql);
    
-$sql="SELECT inventory_id,generation,model,brand,core,speed FROM warehouse_information_sheet WHERE create_by_inventory_id='$user_id' ORDER BY inventory_id DESC LIMIT 1";
+$sql="SELECT inventory_id,generation,model,brand,core,speed FROM main_inventory_informations WHERE create_by_inventory_id='$user_id' ORDER BY inventory_id DESC LIMIT 1";
 $result = mysqli_query($connection,$sql);
+echo $sql;
  $id=0;
 while($row = mysqli_fetch_array($result)) {
    $id=$row['inventory_id'];
@@ -80,7 +85,7 @@ while($row = mysqli_fetch_array($result)) {
     QRcode::png($codeContents, $tempDir . '' . $filename . '.png', QR_ECLEVEL_L, 5, 1);
     
 }
-$query = "INSERT INTO `performance_record_table`(
+$query = "INSERT INTO `performance_records`(
     `user_id`,
     `department_id`,
     `qr_number`,
@@ -92,7 +97,7 @@ $query = "INSERT INTO `performance_record_table`(
     )
     VALUES(
     '$user_id',
-    '$department_id',
+    '1',
     '$id',
     'pc scan',
      now(),
@@ -101,8 +106,56 @@ $query = "INSERT INTO `performance_record_table`(
     '1'
     ) ";
     $result = mysqli_query($connection,$query);
+}else{
+    $id=$inventory_id;
+   $generation=strtoupper($generation);
+   $model=strtoupper($model);
+   $brand=strtoupper($brand);
+   $core=strtoupper($core);
+   $speed=strtoupper($speed);
+   $sql="UPDATE
+    `main_inventory_informations`
+SET
+    `brand` = '$brand',
+    `model` = '$model',
+    `processor` = '$processor',
+    `series` = '$series',
+    `core` = '$core',
+    `generation` = '$generation',
+    `mfg` = '$mfg',
+    `speed` = '$speed',
+    `battery` = '$battery',
+    `lcd_size` = '$lcd_size',
+    `touch_or_none_touch` = '$screen_type',
+    `optical` = '$dvd',
+    `screen_resolution` = '$resolution'
+    WHERE 
+    inventory_id='$inventory_id'";
+     $result = mysqli_query($connection,$sql); 
+   $query = "INSERT INTO `performance_records`(
+    `user_id`,
+    `department_id`,
+    `qr_number`,
+    `job_description`,
+    `start_time`,
+    `end_time`,
+    `target`,
+    status
+    )
+    VALUES(
+    '$user_id',
+    '1',
+    '$id',
+    'pc scan update',
+     now(),
+     now(),
+    '1',
+    '1'
+    ) ";
+    $result = mysqli_query($connection,$query); 
+}
 // (A) IMAGE OBJECT
-$width = 500; $height = 220;
+$width = 400; $height = 215;
 $img = imagecreate($width, $height);
  
 // (B) SETTINGS
@@ -116,10 +169,10 @@ imagefilledrectangle($img, 0, 0, $width, $height, $red);
  
 // (D) WRITE TEXT
 // imagestring($img, $txtSize, $txtX, $txtY, $txt, $white);
- imagestring($img, 12, 10, 10, "$brand $model ", $white);
-   imagestring($img, 12, 10, 30, "$core $speed", $white);
-   imagestring($img, 9, 10, 180, "ALSAKB$id", $white);
-    imagestring($img, 9, 10, 195, "WH2-$generation-$model", $white);
+ imagestring($img, 15, 10, 10, "$brand $model ", $white);
+   imagestring($img, 15, 10, 30, "$core $speed", $white);
+   imagestring($img, 12, 10, 180, "ALSAKB $id", $white);
+    imagestring($img, 12, 10, 195, "WH2-$generation-$model", $white);
  
 // (E) SAVE TO FILE
 imagepng($img, "files/demo1.png");
@@ -139,6 +192,11 @@ imagecopymerge($dest, $src, 15, 50, 0, 0, 110, 110, 75);
 // Output and free from memory
 header('Content-Type: image/png');
 imagegif($dest,"files/demo22.png");
-header("Location: samples.php?test=1");
+if($inventory_id !=0){
+header("Location: samples.php?inventory=1");
+}else{
+header("Location: samples.php");
+}
+
 // imagedestroy($dest);
 // imagedestroy($src);
